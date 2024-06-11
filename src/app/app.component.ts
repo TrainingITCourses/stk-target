939315344b75cb9d6e0463e0e2fcaee4b30fd7bb
Alberto Basalo <albertobasalo@hotmail.com>
2024-06-11 16:28:53 +0200

    target

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LocationComponent } from '@stk/ui';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [LocationComponent],
+  template: ` <stk-ui-location value="Lisboa" /> `,
 })
 export class AppComponent {
-  title = 'stk-target';
+  place = 'Madrid';
 }
